93da300a3202856772529562e0d506999b2ee473
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-04-24 12:35:31 +0300

    add basic functionality of the app

--- pitch/__init__.py
+++ pitch/__init__.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1234567890'
@@ -12,5 +13,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME']='badruuu.n.97@gmail.com'
+app.config['MAIL_PASSWORD']='Noorbadar79'
+mail = Mail(app)
 
 from pitch import routes
+
