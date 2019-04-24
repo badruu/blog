bdfb5e874bebea256aaace9d7141f788dd15fdb8
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-04-24 18:51:13 +0300

    add config.py

---
+++ pitch/config.py
@@ -0,0 +1,11 @@
+import os
+
+class Config:
+    SECRET_KEY='1234567890'
+    SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+
+    MAIL_SERVER='smtp.googlemail.com'
+    MAIL_PORT=587
+    MAIL_USE_TLS=True
+    MAIL_USERNAME='badruuu.n.97@gmail.com'
+    MAIL_PASSWORD='Noorbadar79'
