80a990fcbd575e726bfd4a5fe4cb7c2ca1b60947
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-04-24 19:13:36 +0300

    modified application structure

--- pitch/__init__.py
+++ pitch/__init__.py
@@ -1,24 +1,38 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from pitch.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '1234567890'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME']='badruuu.n.97@gmail.com'
-app.config['MAIL_PASSWORD']='Noorbadar79'
-mail = Mail(app)
+mail = Mail()
 
-from pitch import routes
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from pitch.users.routes import users
+    from pitch.posts.routes import posts
+    from pitch.main.routes import main
+    from pitch.errors.handlers import errors
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
+
+    return app
+
 
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from pitch import app
+from pitch import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
