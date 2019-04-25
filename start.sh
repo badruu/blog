36c26528ffd08a02176387ce7a3ef611533ccdd1
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-04-25 11:29:02 +0300

    add procfile, manage.py, start.sh, requirements.txt

---
+++ start.sh
@@ -0,0 +1,11 @@
+export SECRET_KEY='1234567890'
+
+SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+
+MAIL_SERVER='smtp.googlemail.com'
+MAIL_PORT=587
+MAIL_USE_TLS=True
+MAIL_USERNAME='badruuu.n.97@gmail.com'
+MAIL_PASSWORD='Noorbadar79'
+
+python3.7 manage.py server
